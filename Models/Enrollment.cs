7f9520896d21242e300b9634c7367fa1088274ef
kamalsharma <kamal.sharma01@nagarro.com>
2019-10-31 10:27:29 +0530

    Add project files.

---
+++ Models/Enrollment.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFOperation.Models
+{
+
+    public enum Grade
+    {
+
+        A,B,C,D,E,F
+    }
+    public class Enrollment
+    {
+        public int EnrollmentId { get; set; }
+
+        public int CourseID { get; set; }
+
+        public int StudentID { get; set; }
+
+        public Grade? Grade { get; set; }
+
+        public Student Student { get; set; }
+
+        public Course Course { get; set; }
+
+
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFOperation.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime EnrolledDate { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
